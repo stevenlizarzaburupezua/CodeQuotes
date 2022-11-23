b4cd04137e565bdb97d8dd7caaf59b642ca29ef2
Steven Lizarzaburu Pezúa <lizarzaburupezuasteven@gmail.com>
2022-11-23 10:40:33 -0500

    Generando Background Aleatorio

--- CodeQuotes/MainPage.xaml.cs
+++ CodeQuotes/MainPage.xaml.cs
@@ -3,10 +3,44 @@
 public partial class MainPage : ContentPage
 {
 
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+    public MainPage()
+    {
+        InitializeComponent();
+    }
 
+    Random random = new Random();
+
+    private void btnGenerateQuote_Clicked(object sender, EventArgs e)
+    {
+        var startColor = System.Drawing.Color.FromArgb(
+            random.Next(0, 256),
+            random.Next(0, 256),
+            random.Next(0, 256));
+
+        var endColor = System.Drawing.Color.FromArgb(
+        random.Next(0, 256),
+        random.Next(0, 256),
+        random.Next(0, 256));
+
+        var colors = ColorUtility.ColorControls.GetColorGradient(startColor,
+            endColor, 6);
+
+        float stopOffset = .0f;
+        var stops = new GradientStopCollection();
+
+        foreach (var c in colors)
+        {
+            stops.Add(new GradientStop(Color.FromArgb(c.Name),
+                stopOffset));
+            stopOffset += .2f;
+        }
+
+        var gradient = new LinearGradientBrush(stops,
+            new Point(0, 0),
+            new Point(1, 1));
+
+        background.Background = gradient;
+
+    }
 }
 
